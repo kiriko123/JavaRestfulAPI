14a8e878ce09414fb92d6ffe3d7d396d7b823a41
Tan Khang <tankhang101a@gmail.com>
2024-08-08 22:15:10 +0700

    role

--- src/main/java/com/example/hoidanit/configuration/PermissionInterceptor.java
+++ src/main/java/com/example/hoidanit/configuration/PermissionInterceptor.java
@@ -1,11 +1,27 @@
 package com.example.hoidanit.configuration;
 
+import com.example.hoidanit.model.Permission;
+import com.example.hoidanit.model.Role;
+import com.example.hoidanit.model.User;
+import com.example.hoidanit.service.UserService;
+import com.example.hoidanit.util.SecurityUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 public class PermissionInterceptor implements HandlerInterceptor {
+
+    private final UserService userService;
+
+    @Transactional
     @Override
     public boolean preHandle(
             HttpServletRequest request,
@@ -18,6 +34,27 @@ public class PermissionInterceptor implements HandlerInterceptor {
         System.out.println(">>> path= " + path);
         System.out.println(">>> httpMethod= " + httpMethod);
         System.out.println(">>> requestURI= " + requestURI);
+
+        //check permission
+        String email = SecurityUtil.getCurrentUserLogin().orElse("");
+        if(!email.isEmpty()){
+            User user = userService.findByEmail(email);
+            if(user != null){
+                Role role = user.getRole();
+                if(role != null){
+                    List<Permission> permissions = role.getPermissions();
+                    boolean isAllow= permissions.stream().anyMatch(item ->
+                        item.getApiPath().equals(path) && item.getMethod().equals(httpMethod)
+                    );
+                    System.out.println("This is is allow: " + isAllow);
+                    if (!isAllow){
+                        throw new AccessDeniedException("Access denied");
+                    }
+                }else{
+                    throw new AccessDeniedException("Access denied");
+                }
+            }
+        }
         return true;
     }
 }
--- src/main/java/com/example/hoidanit/configuration/PermissionInterceptorConfiguration.java
+++ src/main/java/com/example/hoidanit/configuration/PermissionInterceptorConfiguration.java
@@ -1,15 +1,21 @@
 package com.example.hoidanit.configuration;
 
+import com.example.hoidanit.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class PermissionInterceptorConfiguration implements WebMvcConfigurer {
+
+    private final UserService userService;
+
     @Bean
     PermissionInterceptor getPermissionInterceptor() {
-        return new PermissionInterceptor();
+        return new PermissionInterceptor(userService);
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
